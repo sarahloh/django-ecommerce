d77353613965c117697570519412968b8b459e76
Sarah Loh <sarah.loh0708@gmail.com>
2018-10-07 20:40:32 +0100

    Add products app

---
+++ products/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from .models import Product
+
+class ProductTests(TestCase):
+    """
+    Tests to run againt Product model
+    """
+
+    def test_str(self):
+        test_name = Product(name='A product')
+        self.assertEqual(str(test_name), 'A product')
